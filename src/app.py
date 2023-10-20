ba34df498706ac1427fb863f3c34b0aba68f5126
venvox <venvoo@icloud.com>
2023-10-20 23:09:04 +0000

    init

--- src/app.py
+++ src/app.py
@@ -27,3 +27,36 @@ chart_non_normalized = alt.Chart(long_df).mark_area().encode(
 st.altair_chart(chart_non_normalized, use_container_width=True)
 
 category_df = pd.read_csv('data/tvl/cache/category.csv')
+chain_counts = category_df['chain'].value_counts()
+filtered_chain_counts = chain_counts[chain_counts >= 10]
+
+# Reset index and rename columns for clarity
+chain_df = filtered_chain_counts.reset_index()
+chain_df.columns = ['Chain', 'ProjectCount']
+
+chain_chart = alt.Chart(chain_df).mark_bar().encode(
+    x=alt.X('Chain:O', title='Chain', sort='-y'),
+    y=alt.Y('ProjectCount:Q', title='Number of Projects'),
+    tooltip=['Chain', 'ProjectCount']
+).properties(
+    title='Distribution of Projects Across Different Chains',
+    width=600,
+    height=400
+)
+
+st.write("### Distribution of Projects Across Different Chains")
+st.altair_chart(chain_chart, use_container_width=True)
+
+percentage_distribution = (filtered_chain_counts / filtered_chain_counts.sum()) * 100
+percentage_chart = alt.Chart(pd.DataFrame(percentage_distribution.reset_index())).mark_bar().encode(
+    x=alt.X('index:O', title='Chain', sort='-y'),
+    y=alt.Y('0:Q', title='Percentage of Projects (%)'),
+    tooltip=['index', '0']
+).properties(
+    title='Percentage Distribution of Projects Across Different Chains',
+    width=600,
+    height=400
+)
+
+st.write("### Percentage Distribution of Projects Across Different Chains")
+st.altair_chart(percentage_chart, use_container_width=True)
