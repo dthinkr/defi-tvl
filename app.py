f9274f53a502c33539ef4ee7d0bb16c48b01df53
venvox <venvoo@icloud.com>
2023-12-06 22:24:39 +0000

    init

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ def load_token_distribution(_bq: BigQueryClient, token_name: str, granularity: s
     
     return token_distribution_df, table_a_df
 
+@st.cache_data
+def load_protocol_data(_bq: BigQueryClient, protocol_name: str, granularity: str):
+    """Load protocol data from BigQuery based on the protocol name and granularity."""
+    return _bq.get_protocol_data(protocol_name, granularity)
+
 @st.cache_resource
 def plot_time_series(data, x_axis, y_axis, color_category):
 
@@ -72,28 +77,35 @@ def plot_time_series(data, x_axis, y_axis, color_category):
 
 def main():
     """Execute the Streamlit app."""
-    st.title("DeFi TVL: Token Distribution Analysis")
+    st.title("DeFi TVL Demo")
+    st.write("# Token Analysis")
     
     bq = BigQueryClient()
+
 
     # Selection for the user to define the data granularity
     granularity = st.selectbox("Select data granularity:", options=['daily', 'weekly', 'monthly'], index=2)  # default to 'weekly'
 
+    # tb = bq.get_data_with_type('C', granularity)
+    # tb.to_csv('tb.csv', index=False)
+
     # User input for selecting a token
     token_name = st.text_input("Enter the token name for analysis (e.g., DAI):", 'USDC')
-
+    
     if token_name:
         # Fetch data for the specified token name with the user-defined percentage and granularity
         token_distribution_df, table_a_df = load_token_distribution(bq, token_name, granularity)
 
-        # token_distribution_df = token_distribution_df.sample(frac=0.1)
-
         token_distribution_df['aggregated_date'] = pd.to_datetime(token_distribution_df['aggregated_date'])
         token_distribution_df['aggregated_date'] = token_distribution_df['aggregated_date'].dt.strftime('%Y-%m-%d')
+
+        # token_distribution_df.to_csv(f'distribution {token_name}.csv', index=False)
 
         # Convert DataFrame to JSON-serializable format (list of dictionaries)
         data_for_observable = token_distribution_df.to_dict(orient='records')
 
+        st.write('## Where is all the token stored?')
+        st.write('### Tree map')
         # Pass this data to the Observable component
         observable("Tree", 
                 notebook="@venvox-ws/defi-tvl-data-loading", 
@@ -121,41 +133,58 @@ def main():
         # Convert the DataFrame to a dictionary
         extracted_df = extracted_df.to_dict(orient='records')
 
+        st.write('### Bar Chart Race')
+
         # Pass this data to the Observable component
         observable("Race", 
                 notebook="@venvox-ws/bar-chart-race", 
                 targets=["chart"],
                 redefine={"data2": extracted_df,})
         
-        if token_distribution_df is not None and not token_distribution_df.empty:
-            # Display the DataFrame and the time series plot for the token
-            st.write(f"#### Retrieved data has {len(token_distribution_df)} rows")
-            st.dataframe(token_distribution_df.head())
-            st.write(f"#### Where is {token_name} locked? By protocol")
-            ts_chart = plot_time_series(token_distribution_df, 'aggregated_date:T', 'total_value_usd:Q', 'protocol_name:N')
-            st.altair_chart(ts_chart, use_container_width=True)
+        st.write('### Time Series')
         
-            # Dropdown for the user to select the aggregation attribute based on table A columns
-            if table_a_df is not None and not table_a_df.empty:
-                aggregation_attribute = st.selectbox("Select an attribute from Table A for grouping:", 
-                                                     options=table_a_df.columns.tolist(), 
-                                                     index=table_a_df.columns.tolist().index('chain') 
-                                                     if 'category' in table_a_df.columns.tolist() else 0)                
-                merged_df = pd.merge(token_distribution_df, table_a_df[['id', aggregation_attribute]], on='id')
+        # Dropdown for the user to select the aggregation attribute based on table A columns
+        if table_a_df is not None and not table_a_df.empty:
+            aggregation_attribute = st.selectbox("Select an attribute from Table A for grouping:", 
+                                                options=table_a_df.columns.tolist(), 
+                                                index=table_a_df.columns.tolist().index('type') 
+                                                if 'category' in table_a_df.columns.tolist() else 0)
 
-                # Merge the two dataframes on the common id ('id')
-                merged_df = pd.merge(token_distribution_df, table_a_df[['id', aggregation_attribute]], on='id')
+            # Merge the two dataframes on the common 'id', adding suffixes to distinguish columns with the same name
+            merged_df = pd.merge(token_distribution_df, table_a_df[['id', aggregation_attribute]], on='id', suffixes=('_token', '_a'))
 
-                # Group by the selected attribute and date, and then sum up the total value
-                merged_agg = merged_df.groupby([aggregation_attribute, 'aggregated_date']).sum(numeric_only=True).reset_index()
+            # Handle renamed columns if 'aggregation_attribute' exists in both DataFrames
+            if aggregation_attribute in token_distribution_df.columns:
+                aggregation_attribute += '_a'  # Use the suffix to refer to the column from table_a_df
 
-                st.write(f"#### Where is {token_name} locked? By {aggregation_attribute}")
-                ts_chart_a = plot_time_series(merged_agg, 'aggregated_date:T', 'total_value_usd:Q', f'{aggregation_attribute}:N')
-                st.altair_chart(ts_chart_a, use_container_width=True)
+            # Group by the selected attribute and date, and then sum up the total value
+            merged_agg = merged_df.groupby([aggregation_attribute, 'aggregated_date']).sum(numeric_only=True).reset_index()
+
+            st.write(f"#### Where is {token_name} locked? By {aggregation_attribute}")
+            ts_chart_a = plot_time_series(merged_agg, 'aggregated_date:T', 'total_value_usd:Q', f'{aggregation_attribute}:N')
+            st.altair_chart(ts_chart_a, use_container_width=True)
+
 
                 
         else:
             st.write("No data available for the specified token.")
+
+    st.write("# Protocol Analysis")
+    # User input for selecting a protocol
+    protocol_name = st.text_input("Enter the protocol name for analysis:", 'MakerDAO')
+
+    if protocol_name:
+        # Fetch data for the specified protocol using the cached function
+        protocol_data_df = load_protocol_data(bq, protocol_name, granularity)
+
+        if protocol_data_df is not None and not protocol_data_df.empty:
+            # Code to process and plot the data
+            st.write(f"#### Token Distribution in {protocol_name} Protocol")
+            ts_chart = plot_time_series(protocol_data_df, 'aggregated_date:T', 'total_value_usd:Q', 'token_name:N')
+            st.altair_chart(ts_chart, use_container_width=True)
+        else:
+            st.write("No data available for the specified protocol.")
+
 
 if __name__ == "__main__":
     main()
