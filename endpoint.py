f93fb814c3aaf56be4a15a2c6cc6c483d7065b13
dthinkr <venvoo@icloud.com>
2024-02-27 23:06:19 +0000

    Add endpoint to retrieve network data with options for top connections and data representation mode

--- endpoint.py
+++ endpoint.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query, Path
 from src.etl_network import ETLNetwork
 from config.query import BigQueryClient 
 from datetime import datetime
@@ -9,9 +9,16 @@ app = FastAPI()
 bq = BigQueryClient()
 etl_network = ETLNetwork(bq=bq)
 
-
-@app.get("/network-json/{year_month}")
-async def get_network_json(year_month: str, TOP_X: int = 50, mode: str = 'usd'):
+@app.get("/network-json/{year_month}", summary="Retrieve Network Data",
+         description="Retrieves a JSON representation of the network data for a given month and year, with options to specify the number of top connections and the mode of data representation.")
+async def get_network_json(
+    year_month: str = Path(..., description="The year and month for which to retrieve network data, in the format 'YYYY-MM'."),
+    TOP_X: int = Query(50, description="The number of top connections to include in the network data."),
+    mode: str = Query('usd', description="The mode of data representation. Can be 'usd' for United States Dollar value or 'qty' for quantity.")
+):
+    """
+    This endpoint dynamically generates network data based on transactions within the specified month, extending to the next month. It processes the data to map tokens to protocols, adjust transaction flows, and prepare the data for visualization.
+    """
     try:
         start_date = datetime.strptime(year_month, "%Y-%m")
         end_date = start_date + relativedelta(months=+1)
